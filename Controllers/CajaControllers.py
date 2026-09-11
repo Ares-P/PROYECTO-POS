4a7cd6bad3db8f9e7f4b1b8e8d1efda3d08f66b3
yinaid-prog979 <yinaid411@gmail.com>
2026-09-11 16:04:26 -0500

    Actualizando archivos

--- Controllers/CajaControllers.py
+++ Controllers/CajaControllers.py
@@ -13,11 +13,19 @@ class CajaControllers:
         result = CajaServices.add(body_data)
         return jsonify({"Mensaje": "Caja actualizada correctamente", "data": result}), 200
 
-    def update(id):
-        body_data = request.get_json()
-        result = CajaServices.update(id, body_data)
-        return jsonify({"Mensaje": "Caja actualizada correctamente", "data": result}), 200
+    def update(uuid):
+        body_data = request.get_json(silent=True)
+        if not body_data:
+            return jsonify({"Mensaje": "el cuerpo esta vacio o invalido"}), 400
 
-    def delete(id):
-        result = CajaServices.delete(id)
-        return jsonify({"Mensaje": "Caja eliminada correctamente", "data": result}), 200
+        result = CajaServices.update(uuid, body_data)
+        if result == 404:
+            return jsonify({"Mensaje": "no se encontro caja"}), 404
+        return jsonify({"mensaje": "Caja actualizado correctamente", "data": result}), 200
+
+    def delete(uuid):
+        x = CajaServices.delete(uuid)
+        if x == 404:
+            return jsonify({"Mensaje": "no se encontro caja"}), x
+        else:
+            return jsonify({"mensaje": "Caja eliminado correctamente"}), x
